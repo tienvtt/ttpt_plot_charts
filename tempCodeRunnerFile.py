0f166193d4dd4f0e5636ebb880435e4d4a731d6d
VoThiThuTien <tienvttt2003@gmail.com>
2025-07-09 11:13:52 +0700

    delete files

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-combinechart(
-    "Y2022",
-    "Y2024",
-    timeframe="yearly",
-    title="Biểu đồ Giá cổ phiếu VCB vs TCB từng năm",
-).add_stock("VCB").add_stock("TCB").plot_bar(save_path="./combine_img")
